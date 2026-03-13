04abcbd42e43bdc19c8c25d7561744c068d3613c
Rafaela <rafaelatanakacardoso@gmail.com>
2026-03-12 21:11:23 -0300

    primeiro commit

---
+++ lista/src/test/java/com/tc/lista/ListaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.lista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
